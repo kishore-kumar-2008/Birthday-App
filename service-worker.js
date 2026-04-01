520e482e132baa45571e72e6c72378f1d863cee8
kishore-kumar-2008 <kishorekumarsiva7@gmail.com>
2026-04-01 10:39:59 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", () => {
-    console.log("Service Worker Installed");
-});
-
-self.addEventListener("activate", () => {
-    console.log("Service Worker Activated");
-});
